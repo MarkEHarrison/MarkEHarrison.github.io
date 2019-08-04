c7b8d7afec707dce8749dbc34cac14bf7fb42f9e
Angel Harrison <mharrison11b@gmail.com>
2019-08-04 11:18:41 -0700

    index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,199 +1,83 @@
-$(document).ready(function () {
-    "use strict";
-    /* === Footer Instagram === */
-    (function () {
-        $('#footer-instagram').slick({
-            infinite: true,
-            speed: 300,
-            slidesToShow: 8,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 6,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 4,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
-        });
-    }());
-    /* === menu drop-down === */
-    (function () {
-        $('#food-feature-carousel').slick({
-            infinite: true,
-            speed: 300,
-            slidesToShow: 3,
-            prevArrow: '<i class="fa fa-angle-left left"></i>',
-            nextArrow: '<i class="fa fa-angle-right right"></i> ',
-            responsive: [
+/*
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-                {
-                    breakpoint: 800,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
-        });
-    }());
-    (function () {
-        $('.related-post-carousel-items').slick({
-            infinite: true,
-            speed: 300,
-            slidesToShow: 3,
-            autoPlay: true,
-            responsive: [
-                {
-                    breakpoint: 800,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
-        });
-    }());
-    (function () {
-        $('.archi-feature-category').slick({
-            centerMode: true,
-            slidesToShow: 3,
-            centerPadding: '60px',
-            prevArrow: '<i class="fa fa-angle-left left"></i>',
-            nextArrow: '<i class="fa fa-angle-right right"></i> ',
-            responsive: [
+(function($) {
 
-                {
-                    breakpoint: 800,
-                    settings: {
-                        centerMode: true,
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        centerMode: true,
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
-        });
-    }());
-    (function () {
-        $('.popular-post-slider').slick({
-            infinite: true,
-            arrows: false,
-            speed: 300,
-            slidesToShow: 4,
-            slidesToScroll: 2,
-            dots: true,
-            responsive: [
-                {
-                    breakpoint: 800,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        dots: true
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        dots: true
-                    }
-                }
-            ]
-        });
-    }());
-    /* === Search === */
-    (function () {
-        $('.top-search a').click(function (e) {
-            e.preventDefault();
-            //when the notification icon is clicked open the menu
-            $(this).toggleClass('active');
-            $('.show-search').fadeToggle(function () {
-                //then bind the close event to html so it closes when you mouse off it.
-                $('html').bind('click', function () {
-                    $('.show-search').fadeOut(function () {
-                        //once html has been clicked and the menu has closed, unbind the html click so nothing else has to lag up
-                        $('html').unbind('click');
-                    });
-                    $('.top-search a').removeClass('active');
-                });
-                $('.show-search').bind('click', function (e) {
-                    e.stopPropagation();
-                });
-            });
-        });
-    }());
-    (function () {
-        if (screen.width > 768) {
-            var $dropdown = $(".nav .dropdown");
-            $dropdown.mousemove(function () {
-                $(this).addClass("open");
-            });
-            $dropdown.mouseleave(function () {
-                $dropdown.removeClass("open");
-            });
-        }
-        $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function (event) {
-            // Avoid following the href location when clicking
-            event.preventDefault();
-            // Avoid having the menu to close when clicking
-            event.stopPropagation();
-            // Re-add .open to parent sub-menu item
-            $(this).parent().addClass('open');
-            $(this).parent().find("ul").parent().find("li.dropdown").toggle('open');
-            /* $(this).parent().find("ul").parent().find("li.dropdown").removeClass('open');*/
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#page-wrapper'),
+		$banner = $('#banner'),
+		$header = $('#header');
 
-        });
-    }());
-    //scroll top
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ null,      '480px'  ]
+		});
 
-    (function () {
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 100) {
-                $('.scroll-up').fadeIn();
-            } else {
-                $('.scroll-up').fadeOut();
-            }
-        });
-        $('.scroll-up a').click(function () {
-            $('html, body').animate({scrollTop: 0}, 800);
-            return false;
-        });
-    }());
-}());
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
+
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
+
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
+
+		}
+
+	// Scrolly.
+		$('.scrolly')
+			.scrolly({
+				speed: 1500,
+				offset: $header.outerHeight()
+			});
+
+	// Menu.
+		$('#menu')
+			.append('<a href="#menu" class="close"></a>')
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right',
+				target: $body,
+				visibleClass: 'is-menu-visible'
+			});
+
+	// Header.
+		if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
+
+			$window.on('resize', function() { $window.trigger('scroll'); });
+
+			$banner.scrollex({
+				bottom:		$header.outerHeight() + 1,
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
+			});
+
+		}
+
+})(jQuery);
